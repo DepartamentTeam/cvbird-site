1fef5695aed62b9d924aa25064915f37f3438e68
Емельянов Константин Валентинович <emelyanovkonstantin@yandex.ru>
2023-10-30 21:28:28 +0300

    Initialization of the Project

---
+++ src/main/java/ai/cvbird/cvbirdsite/CvbirdSiteApplication.java
@@ -0,0 +1,13 @@
+package ai.cvbird.cvbirdsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvbirdSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvbirdSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/cvbird/cvbirdsite/CvbirdSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.cvbird.cvbirdsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvbirdSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
