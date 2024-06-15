21deebec9f60e5b9f6214effa0ca166ae2d41434
Емельянов Константин Валентинович <emelyanovkonstantin@yandex.ru>
2024-06-15 06:18:29 +0300

    feat: fix VacancyController

--- src/main/java/ai/cvbird/cvbirdsite/controller/VacancyController.java
+++ src/main/java/ai/cvbird/cvbirdsite/controller/VacancyController.java
@@ -1,11 +1,16 @@
 package ai.cvbird.cvbirdsite.controller;
 
+import ai.cvbird.cvbirdsite.dto.CVBirdUserResponse;
+import ai.cvbird.cvbirdsite.dto.StringResponse;
 import ai.cvbird.cvbirdsite.dto.Vacancy;
 import ai.cvbird.cvbirdsite.exception.NotEnoughFundsException;
 import ai.cvbird.cvbirdsite.model.CVBirdUser;
 import ai.cvbird.cvbirdsite.service.TelegramService;
 import ai.cvbird.cvbirdsite.service.VacancyService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,10 +37,11 @@ public class VacancyController {
 
     @Operation(summary = "Get list of recommended vacancies by CVBirdUserId by Telegram ID")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Vacancies have been found"),
+            @ApiResponse(responseCode = "200", description = "Vacancies have been found",content = { @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = Vacancy.class))) }),
             @ApiResponse(responseCode = "422", description = "There are not enough funds on your balance")})
     @GetMapping(value = "/get_by_telegram_id/{telegramId}")
-    public ResponseEntity<List<Vacancy>> getCVByTelegramId(@PathVariable String telegramId){
+    public ResponseEntity<?> getCVByTelegramId(@PathVariable String telegramId){
         CVBirdUser cvBirdUser = telegramService.getCVBirdUser(telegramId);
         if (cvBirdUser != null){
             try {
@@ -46,7 +52,8 @@ public class VacancyController {
             }
 
         }
-        return new ResponseEntity<>(null, HttpStatus.OK);
+        StringResponse stringResponse = new StringResponse("There is no such user telegramId");
+        return new ResponseEntity<>(stringResponse, HttpStatus.OK);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
